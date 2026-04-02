023bd22fc26b823a414742e7d3edd7b7b9565f3e
KaushikNv <114083926+KaushikNv@users.noreply.github.com>
2026-04-02 09:29:24 +0530

    Add files via upload

---
+++ DropTable.sql
@@ -0,0 +1,7 @@
+DROP TABLE sales;
+
+DROP TABLE customers;
+
+DROP TABLE items;
+
+DROP TABLE companies;
---
+++ OR.sql
@@ -0,0 +1,22 @@
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Denis';
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Denis'OR first_name = 'Elvis'
+;
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Kellie'OR first_name = 'Aruna'
+;
