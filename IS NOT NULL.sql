93bd0d364ee8e7ac8b9b32b77e196f558b87a9bb
KaushikNv <114083926+KaushikNv@users.noreply.github.com>
2026-04-22 09:16:26 +0530

    Add files via upload

---
+++ IS NOT NULL.sql
@@ -0,0 +1,16 @@
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name IS NOT NULL;
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name IS NULL;
+    
+SELECT * FROM departments WHERE dept_no IS NOT NULL;
+SELECT * FROM departments WHERE dept_no IS NULL;
---
+++ Other Comparison Operators.sql
@@ -0,0 +1,8 @@
+SELECT * FROM employees WHERE first_name = 'Mark';
+SELECT * FROM employees WHERE first_name != 'Mark';
+SELECT * FROM employees WHERE first_name <> 'Mark';
+
+SELECT * FROM employees WHERE hire_date > '2000-01-01';
+SELECT * FROM employees WHERE hire_date >= '2000-01-01';
+SELECT * FROM employees WHERE hire_date <'1985-02-01';
+SELECT * FROM employees WHERE hire_date >= '2000-01-01' AND gender = 'F'; SELECT * FROM salaries WHERE salary > 150000;
